52cfdffae8ab5856f542c5445cf093daf5031f6e
Andrii Podkolzin <andrii.podkolzin@namecheap.com>
2023-04-19 10:34:35 +0300

    Split into assemblies

---
+++ DevJungles.Assembler/AsmCommand.cs
@@ -0,0 +1,28 @@
+﻿namespace DevJungles.Assembler;
+
+public enum Commands : byte
+{
+  Add,
+  Sub,
+  Lt,
+  Gt,
+  Jmp,
+  Push,
+  Read, 
+  Write,
+  Put,
+  Print,
+}
+
+public struct AsmCommand // 12 bytes like Arm x32
+{
+  public Commands Command;
+  public byte Register1;
+  public byte Register2;
+  public byte Register3;
+
+  public int LeftOperand;
+  public int RightOperand;
+
+  public string Variable; // TEMP SOLUTION. Will be removed
+}
---
+++ DevJungles.Language/Command.cs
@@ -0,0 +1,34 @@
+﻿using DevJungles.Assembler;
+
+namespace DevJungles.Language;
+
+public interface ICommand
+{
+  AsmCommand ToAsmCommand();
+}
+
+public class Command : ICommand
+{
+  private readonly AsmCommand _cmd;
+  private Command(AsmCommand cmd)
+  {
+    _cmd = cmd;
+  }
+
+  public AsmCommand ToAsmCommand() => _cmd;
+
+  public static ICommand Push() => new Command(new() { Command = Commands.Push});
+
+  public static ICommand Put(byte regNumber, int constant) => new Command(new () { Command = Commands.Put, Register1 = regNumber, LeftOperand = constant });
+
+  public static ICommand Print(string text) => new Command(new () { Command = Commands.Print });
+  public static ICommand Add(byte regNumber) => new Command(new () { Command = Commands.Add, Register1 = regNumber });
+  public static ICommand Sub(byte regNumber) => new Command(new () { Command = Commands.Sub, Register1 = regNumber });
+  public static ICommand Lt(byte regNumber) => new Command(new () { Command = Commands.Lt, Register1 = regNumber });
+  public static ICommand Gt(byte regNumber) => new Command(new () { Command = Commands.Gt, Register1 = regNumber });
+  
+  public static ICommand Jmp() => new Command(new () { Command = Commands.Jmp });
+    
+  public static ICommand Read(byte regNumber, int stackAddress) => new Command(new () { Command = Commands.Read, Register1 = regNumber, LeftOperand = stackAddress });
+  public static ICommand Write(byte regNumber, int stackAddress) => new Command(new () { Command = Commands.Write, Register1 = regNumber, LeftOperand = stackAddress });
+}
