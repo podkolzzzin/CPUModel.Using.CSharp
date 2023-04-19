67d8f10c45ae49614e6fa0b82881b5792b2db636
Andrii Podkolzin <andrii.podkolzin@namecheap.com>
2023-04-19 10:05:44 +0300

    Minimal usage of characters

--- DevJungles.VirtualMachine/Class1.cs
+++ DevJungles.VirtualMachine/Class1.cs
@@ -1,13 +1,44 @@
 ﻿namespace DevJungles.VirtualMachine;
 
-public class OperationSystem { }
+public class OperationSystem
+{
+  public Process CreateProcess(Assembly assembly)
+  {
+    return new Process(assembly);
+  }
+}
 
 public class MemoryManager { }
 
-public class Process {}
+public class Process
+{
+  public Thread MainThread { get; set; }
+  public Process(Assembly assembly)
+  {
+    MainThread = new Thread(GetEntryPoint(assembly));
+  }
+  private object GetEntryPoint(Assembly assembly)
+  {
+    return new object();
+  }
+}
 
-public class Thread {}
+public class Thread
+{
+  public Thread(object entryPoint)
+  {
+    
+  }
+}
 
-public class Compiler {}
+public class Compiler
+{
+  public Assembly Compile(Source source)
+  {
+    return new Assembly();
+  }
+}
 
 public class Assembly {}
+
+public class Source {}
